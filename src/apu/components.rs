52f59190bf08b5fb3d107074eb9f662af5bf8a16
Michael Louis Thaler <mthaler@managedbyq.com>
2019-06-20 13:06:32 -0400

    Reorganize APU code

---
+++ src/apu/components.rs
@@ -0,0 +1,89 @@
+use crate::common::Clocked;
+use crate::apu::Channel;
+
+pub const LENGTH_COUNTER_TABLE: [u8; 32] = [
+    10, 254, 20, 2,  40, 4,  80, 6,  160, 8,  60, 10, 14, 12, 26, 14,  // 00 - 0F
+    12, 16,  24, 18, 48, 20, 96, 22, 192, 24, 72, 26, 16, 28, 32, 30   // 10 - 1F
+];
+
+pub struct LengthCounter {
+    pub length: u8,
+    pub halt: bool
+}
+
+impl Clocked for LengthCounter {
+    fn tick(&mut self) {
+        if !self.halt && self.length > 0 {
+            self.length -= 1;
+        }
+    }
+}
+
+impl LengthCounter {
+    pub fn new() -> LengthCounter {
+        LengthCounter { length: 0, halt: false }
+    }
+
+    pub fn silenced(&self) -> bool {
+        self.halt && self.length == 0
+    }
+}
+
+pub struct Envelope {
+    pub start: bool,
+    looping: bool,
+    constant: bool,
+    decay: u8,
+    value: u8,
+    current: u8
+}
+
+impl Envelope {
+    pub fn new() -> Envelope {
+        Envelope {
+            start: false,
+            looping: false,
+            constant: false,
+            decay: 0,
+            value: 0,
+            current: 0,
+        }
+    }
+}
+
+impl Clocked for Envelope {
+    fn tick(&mut self) {
+        // https://wiki.nesdev.com/w/index.php/APU_Envelope
+        if self.start {
+            self.start = false;
+            self.current = 0xF;
+            self.decay = self.value;
+        } else if self.decay > 0 {
+            self.decay -= 1;
+        } else {
+            if self.current > 0 {
+                self.current -= 1;
+            } else if self.looping {
+                self.current = 0xF;
+            }
+            self.decay = self.value;
+        }
+    }
+}
+
+impl Channel for Envelope {
+    fn set_register(&mut self, _addr: u16, value: u8) {
+        self.value = value & 0b0000_1111;
+        self.current = self.value;
+        self.looping = (value & 0b0010_0000) != 0;
+        self.constant = (value & 0b0001_0000) != 0;
+    }
+
+    fn sample(&mut self) -> Option<f32> {
+        if self.constant {
+            Some(self.value as f32)
+        } else {
+            Some(self.current as f32)
+        }
+    }
+}
---
+++ src/apu/pulse.rs
@@ -0,0 +1,94 @@
+use crate::apu::Channel;
+use crate::apu::components::{Envelope, LengthCounter, LENGTH_COUNTER_TABLE};
+use crate::common::Clocked;
+
+enum Duty {
+    Eighth,
+    Fourth,
+    Half,
+    ThreeFourths
+}
+
+impl Duty {
+    fn waveform(&self) -> u8 {
+        match self {
+            Duty::Eighth =>       0b0100_0000,
+            Duty::Fourth =>       0b0110_0000,
+            Duty::Half =>         0b0111_1000,
+            Duty::ThreeFourths => 0b1001_1111,
+        }
+    }
+
+    fn active(&self, step: u8) -> bool {
+        self.waveform() >> (7 - (step & 0b0000_0111)) & 1 != 0
+    }
+}
+
+pub struct Pulse {
+    duty: Duty,
+    step: u8,
+    timer: u16,
+    period: u16,
+
+    pub envelope: Envelope,
+    pub length_counter: LengthCounter,
+}
+
+impl Pulse {
+    pub fn new() -> Pulse {
+        Pulse {
+            duty: Duty::Eighth,
+            envelope: Envelope::new(),
+            length_counter: LengthCounter::new(),
+            step: 0,
+            timer: 0,
+            period: 0
+        }
+    }
+}
+
+impl Channel for Pulse {
+    fn set_register(&mut self, addr: u16, value: u8) {
+        match addr & 0b0000_0011 {
+            0 => {
+                self.duty = match value >> 6 {
+                    0 => Duty::Eighth,
+                    1 => Duty::Fourth,
+                    2 => Duty::Half,
+                    3 => Duty::ThreeFourths,
+                    _ => unreachable!()
+                };
+                self.length_counter.halt = (value & 0b0010_0000) == 0;
+                self.envelope.set_register(addr, value);
+            },
+            1 => {},
+            2 => {self.period = self.period & 0xFF00 | value as u16},
+            3 => {
+                self.period = self.period & 0x00FF | (((value & 0b0000_0111) as u16) << 8);
+                self.length_counter.length = LENGTH_COUNTER_TABLE[usize::from(value >> 3)];
+                self.envelope.start = true;
+                self.step = 0;
+            },
+            _ => unreachable!()
+        }
+    }
+
+    fn sample(&mut self) -> Option<f32> {
+        if !self.length_counter.silenced() && self.duty.active(self.step) {
+            self.envelope.sample()
+        } else {
+            None
+        }
+    }
+}
+
+impl Clocked for Pulse {
+    fn tick(&mut self) {
+        if self.timer == 0 {
+            self.step = self.step.wrapping_sub(1) & 0b0000_0111;
+            self.timer = self.period;
+        } else {
+            self.timer -= 1;
+        }
+    }
+}
